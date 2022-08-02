5748e2efee6f32bd50543950fc0370c88d12a307
DenVilk <vvelikovich@gmail.com>
2022-08-02 19:42:21 +0300

    Lesson 2

--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,12 @@
 from django.shortcuts import render
-
-posts = [
-	{
-		'author': 'Max',
-		'title': 'Blog Post 1',
-		'content': 'First post content',
-		'date_posted': 'Feb 25, 2022',
-	},
-	{
-		'author': 'Ian',
-		'title': 'Blog Post 2',
-		'content': 'Second post content',
-		'date_posted': 'Feb 23, 2022',
-	},
-]
-
+from blog.models import Post
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/index.html', context)
+
 
 def about(request):
     return render(
